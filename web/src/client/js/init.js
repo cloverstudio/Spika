fc7bb756c3152d9791df7e4c42952c5ec5144590
root <ken.yasue@clover-studio.com>
2015-09-03 06:07:06 -0400

    Started working on localization

--- web/src/client/js/init.js
+++ web/src/client/js/init.js
@@ -7,6 +7,7 @@
     Config.apiBaseUrl = "http://45.55.81.215/v1";
     Config.socketUrl = "http://45.55.81.215";
     Config.defaultContainer = "#spika-container";
+    Config.lang = "ja";
     Config.showSidebar = true;
     Config.showTitlebar = true;
     Config.thumbnailHeight = 256;
---
+++ web/src/client/js/lang/ja.js
@@ -0,0 +1,7 @@
+var LangTable = {
+    
+    'Type your text ...' : 'メッセージを入力してください。'
+    
+}
+
+module["exports"] = LangTable;
